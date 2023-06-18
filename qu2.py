4c7c5bef665433c67738c1a24e7cbcfb7d588818
ronsh205 <ronsh205@gmail.com>
2023-06-18 15:16:02 +0300

    read from file line by line

--- qu2.py
+++ qu2.py
@@ -7,4 +7,12 @@ for z, x in enumerate(files):
     Dic_lis[z + 1] = x
 
 chose_user = int(input("enter number of line file\n"))
-print(Dic_lis[chose_user])
+print("the file work with is :\n" + Dic_lis[chose_user])
+file_work = Dic_lis[chose_user]
+with open(file_work, 'r') as re:
+    Lines = re.readlines()
+    count = 0
+    # Strips the newline character
+    for line in Lines:
+        count += 1
+        print("Line {}: {}".format(count, line.strip()))
